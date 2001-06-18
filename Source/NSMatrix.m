39fc52b875d0bfe3ff5ba2fdd1025bfca04b2fde
Adam Fedor <fedor@gnu.org>
2001-06-18 23:01:02 +0000

    Call abortEditing
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/gui/branches/freeze-0_7_0@10203 72102866-910b-0410-8b05-ffd578937521

--- Source/NSMatrix.m
+++ Source/NSMatrix.m
@@ -2287,6 +2287,8 @@ static SEL getSel;
   if ([self getRow: &row column: &col ofCell: aCell] == NO)
     return;	// Not a cell in this matrix - we can't update it.
 
+  [self abortEditing];
+
   rect = [self cellFrameAtRow: row column: col];
   [self setNeedsDisplayInRect: rect];
 }
