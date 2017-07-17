13091c63f0b13eb64e01ab669bdbfaa8e3c0e850
Daniel Ferreira <dtf@stanford.edu>
2017-07-18 03:57:18 +1000

    nsuserinterfacelayout stub

---
+++ Headers/AppKit/NSUserInterfaceLayout.h
@@ -0,0 +1,9 @@
+#ifndef NS_KEKTOP
+#define NS_KEKTOP
+
+typedef enum NSUserInterfaceLayoutDirection {
+    NSUserInterfaceLayoutDirectionLeftToRight = 0,
+    NSUserInterfaceLayoutDirectionRightToLeft = 1
+};
+
+#endif
